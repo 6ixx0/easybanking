08a9f0375315eec60e75d045540a27da658147ec
6ixx0 <iraph94@gmail.com>
2021-12-22 10:19:34 -0800

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+// document.getElementsByClassName("links").style.display = "none"
+// document.getElementsByClassName("hamburger").onclick = function name(params){
+//     document.getElementsByClassName("links").style.display = "block"
+//     document.getElementsByClassName("hamburger").style.display = "none"
+// }
+
+
